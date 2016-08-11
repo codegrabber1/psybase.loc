6c5ddff1370130093b7c01cd9c54cea492c3161f
codegrabber1 <poruchenko@ukr.net>
2016-08-11 23:27:47 +0300

    110823_27

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,62 @@
 "use strict";
-const gulp = require('gulp');
+
+const gulp      = require('gulp');
+const bs        = require('browser-sync').create();
+const wiredep   = require('wiredep').stream;
+const sass      = require('gulp-sass');
+const concat    = require('gulp-concat');
+const cached    = require('gulp-cached');
+const multipipe = require('multipipe');
+const notify    = require('gulp-notify');
+const autopref  = require('gulp-autoprefixer');
 
 
-gulp.task('default', function() {
-  // place code for your default task here
+// bower
+gulp.task('bower', function() {
+  return gulp.src('frontend/*.html')
+      .pipe(wiredep({
+        directory : "frontend/bower_components"
+      }))
+      .pipe(gulp.dest('frontend/'));
 });
+
+/*== server ==*/
+gulp.task('server', function () {
+  bs.init({
+    'server': 'frontend'
+  });
+  bs.watch('frontend/**/*.*').on('change', bs.reload);
+});
+
+/*== sass ==*/
+gulp.task('sass',function(){
+    return multipipe(
+        gulp.src('frontend/sass/main.sass'),
+        cached('sass'),
+        sass().on('error', sass.logError),
+        autopref({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }),
+        gulp.dest('frontend')
+    ).on('error', notify.onError());
+});
+
+/*== scripts ==*/
+gulp.task('scripts', function(){
+    return multipipe(
+        gulp.src([
+            'frontend/'
+        ]),
+        cached('scripts'),
+        concat('all.js'),
+        gulp.dest('frontend/js')
+    );
+});
+
+/*== watch ==*/
+gulp.task('watch', function(){
+    gulp.watch('frontend/sass/*.sass',['sass']);
+});
+/*== default ==*/
+gulp.task('default', ['watch']);
